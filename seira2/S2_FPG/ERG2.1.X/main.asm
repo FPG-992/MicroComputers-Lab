40852663ab73e76cccf7909cb6343cb36e3f1128
FilipposGiannakopoulos <78109143+FilipposGiannakopoulos@users.noreply.github.com>
2024-10-14 18:02:42 +0300

    Minor code reorganization

--- seira2/S2_FPG/ERG2.1.X/main.asm
+++ seira2/S2_FPG/ERG2.1.X/main.asm
@@ -1,9 +1,8 @@
-;????? 2.1? ????????? ???????
 .include "m328PBdef.inc"      ; ATmega328P microcontroller definitions
 
 .equ FOSC_MHZ=16              ; Microcontroller operating frequency in MHz
 
-.equ DEL_mS=500               ; Delay in mS (valid number from 1 to 4095)
+.equ DEL_mS=500              ; Delay in mS (valid number from 1 to 4095)
 
 .equ DEL_NU=FOSC_MHZ*DEL_mS   ; Delay_mS routine: (1000*DEL_NU+6) cycles
 
@@ -47,6 +46,37 @@ sei ;enable global interrupts
 
 out PORTC, counter ;show counter in leds
 
+;==== Delay FUNCTION STARTS HERE ====
+
+loop1:
+clr r26
+loop2:
+out PORTB, r26
+ldi r24, low(DEL_NU)          ; Set delay (number of cycles)
+ldi r25, high(DEL_NU)
+rcall delay_mS
+inc r26
+cpi r26, 16                   ; Compare r26 with 16
+breq loop1
+rjmp loop2
+
+
+; Delay of 1000*F1+6 cycles (almost equal to 1000*F1 cycles)
+delay_mS:
+ldi r23, 249                  ; 1 cycle
+
+loop_inn:
+dec r23                       ; 1 cycle
+nop                           ; 1 cycle
+brne loop_inn                 ; 1 or 2 cycles
+sbiw r24, 1                   ; 2 cycles
+brne delay_mS                 ; 1 or 2 cycles
+
+ret                            ; 4 cycles
+
+==== DELAY FUNCTION ENDS HERE ====
+
+
 
 ;======
 ;Counts (INT1=PD3) interrupts from 0 to 63 included
@@ -81,20 +111,8 @@ brne skip_reset
 ldi counter,0
 
 skip_reset:
-;Delay 100 mS
- ldi r24, low(16*100) ; Init r25, r24 for delay 100 mS
- ldi r25, high(16*100) ; CPU frequency = 16 MHz
-delay1:
- ldi r23, 249 ; (1 cycle)
-delay2:
- dec r23 ; 1 cycle
- nop ; 1 cycle
- brne delay2 ; 1 or 2 cycles
- sbiw r24, 1 ; 2 cycles
- brne delay1 ; 1 or 2 cycles
-
- ldi r24,(1<<INTF1)
- out EIFR, r24 ; Clear INTF1 flag
+ldi r24,(1<<INTF1)
+out EIFR, r24 ; Clear INTF1 flag
 
 pop r25
 out SREG, r25
@@ -107,29 +125,3 @@ reti
 ;===============
 ;ISR1 ENDS HERE
 ;===============
-
-loop1:
-clr r26
-loop2:
-out PORTB, r26
-ldi r24, low(DEL_NU)          ; Set delay (number of cycles)
-ldi r25, high(DEL_NU)
-rcall delay_mS
-inc r26
-cpi r26, 16                   ; Compare r26 with 16
-breq loop1
-rjmp loop2
-
-
-; Delay of 1000*F1+6 cycles (almost equal to 1000*F1 cycles)
-delay_mS:
-ldi r23, 249                  ; 1 cycle
-
-loop_inn:
-dec r23                       ; 1 cycle
-nop                           ; 1 cycle
-brne loop_inn                 ; 1 or 2 cycles
-sbiw r24, 1                   ; 2 cycles
-brne delay_mS                 ; 1 or 2 cycles
-
-ret                            ; 4 cycles
