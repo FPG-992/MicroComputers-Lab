6d02363fcb09a8ee89f4229a71ce99aa6e25efd4
FPG-992 <78109143+FPG-992@users.noreply.github.com>
2024-10-24 21:44:21 +0300

    File Structure for 3.2 | 3.3

---
+++ seira3/Filippos_Version/ERG3.2.X/main.c
@@ -0,0 +1,17 @@
+/*
+ * File:   main.c
+ * Author: filip
+ *
+ * Created on October 24, 2024, 9:42 PM
+ */
+
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
