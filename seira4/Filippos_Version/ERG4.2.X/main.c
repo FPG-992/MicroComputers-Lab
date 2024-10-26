138cc68840176e22170bfe7a1ce7414f4f4abf11
FPG-992 <78109143+FPG-992@users.noreply.github.com>
2024-10-26 15:36:35 +0300

    Added C files

---
+++ seira4/Filippos_Version/ERG4.2.X/main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: filip
+ *
+ * Created on 26 ????????? 2024, 3:35 ??
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
