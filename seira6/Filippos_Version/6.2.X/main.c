732c65df27edd28baacce80e35b07b77bb0f5d3f
FPG-992 <78109143+FPG-992@users.noreply.github.com>
2024-11-20 19:41:16 +0200

    Finished EX6.1

---
+++ seira6/Filippos_Version/6.2.X/main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: filip
+ *
+ * Created on November 20, 2024, 6:43 PM
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
